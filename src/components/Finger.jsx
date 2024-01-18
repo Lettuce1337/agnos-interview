3bb6cc18a5749532179d2fe40bb02204bce7ec7a
Sirivarakul <sirivarakuljiropakarn@gmail.com>
2024-01-18 20:26:47 +0700

    Interactable "blob" prototype
    - Added responsive scalable SVG for different parts.

--- src/components/Finger.jsx
+++ src/components/Finger.jsx
@@ -10,7 +10,13 @@ function Finger() {
   return (
     <>
       <p className='text-[2rem] pt-[1rem] max-md:text-[1.5rem] max-[480px]:text-[1rem]'>จุดไหนที่คุณปวดนิ้วมากที่สุด?</p>
-      <img src={finger} className='w-[70%] h-[70%]'/>
+      <div className='flex justify-center items-center relative'>
+        <img src={finger} className='w-[70%] h-[70%]'/>
+        <svg width='12%' height='12%' viewBox="0 0 109 137" fill="none" xmlns="http://www.w3.org/2000/svg" 
+              className='absolute bottom-[52%] right-[45%]'>
+          <path d="M1 69C3 40 22.4 -13.6 84 4C161 26 29 244 1 69Z" stroke="black" fill="white" opacity='70%' onClick={()=>{console.log('click!')}} />
+        </svg>
+      </div>
       <div className='w-full h-1/6 bg-white absolute bottom-0 flex justify-center items-center'>
         <OtherButton text='ข้ออื่นๆ หรือไม่ได้ปวดที่บริเวณข้อ' selected={selected} setSelected={setSelected}/ >
       </div>
--- src/components/Stomach.jsx
+++ src/components/Stomach.jsx
@@ -10,10 +10,17 @@ function Stomach() {
   return (
     <>
       <p className='text-[2rem] pt-[1rem] max-md:text-[1.5rem] max-[480px]:text-[1rem]'>ปวดท้องที่บริเวณใดมากที่สุด?</p>
-      <img src={stomach} className='w-[70%] h-[70%]'/>
+      <div className='flex justify-center items-center relative'>
+        <img src={stomach} className='w-[70%] h-[70%]'/>
+        <svg width='12%' height='12%' viewBox="0 0 109 137" fill="none" xmlns="http://www.w3.org/2000/svg" 
+              className='absolute bottom-[52%] right-[45%]'>
+          <path d="M1 69C3 40 22.4 -13.6 84 4C161 26 29 244 1 69Z" stroke="black" fill="white" opacity='70%' onClick={()=>{console.log('click!')}} />
+        </svg>
+      </div>
       <div className='w-full h-1/6 bg-white absolute bottom-0 flex justify-center items-center'>
         <OtherButton text='ปวดทั่วท้อง' selected={selected} setSelected={setSelected}/ >
       </div>
+
     </>
   )
 }
